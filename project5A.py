2126be98ae535033ccce4b0e1f9a2957a61f1735
Hoang Dang <alex.dang20@outlook.com>
2023-04-12 17:27:27 -0600

    normalize & edge test

--- project5A.py
+++ project5A.py
@@ -78,7 +78,7 @@ try:
 
         blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-        ret, thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY_INV)
+        ret, thresh = cv2.threshold(blurred, 120, 255, cv2.THRESH_BINARY_INV)
 
         mask = cv2.erode(thresh, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
