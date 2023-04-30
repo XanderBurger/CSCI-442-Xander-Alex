c18fcef5b122da233fec9de76296367a1ef7a9d7
Xander Burger <xanburger@hotmail.com>
2023-04-30 12:42:28 -0600

    maybe

--- stateMachine/miningState.py
+++ stateMachine/miningState.py
@@ -46,7 +46,6 @@ class MiningState(State):
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
-        flags=cv2.CV_HAAR_SCALE_IMAGE
         )
         
         faceY = None
