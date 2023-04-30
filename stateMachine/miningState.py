0035391b8be292030905706bab6a8c5e3b7cbafe
Xander Burger <xanburger@hotmail.com>
2023-04-30 12:40:35 -0600

    maybe

--- stateMachine/miningState.py
+++ stateMachine/miningState.py
@@ -40,9 +40,14 @@ class MiningState(State):
         faces = None
         
         gray = cv2.cvtColor(color_frame, cv2.COLOR_BGR2GRAY)
-        mat30 = np.full(gray.shape, 30, dtype=np.uint8)
-        gray = cv2.add(gray, mat30)
-        faces = tango.face_cascade.detectMultiScale(gray, 1, 5)
+        # mat30 = np.full(gray.shape, 30, dtype=np.uint8)
+        # gray = cv2.add(gray, mat30)
+        faces = tango.face_cascade.detectMultiScale(gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
         
         faceY = None
         faceX = None
