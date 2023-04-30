a14af4474a94c2ad17b0701f2e0874b944e84492
Xander Burger <xanburger@hotmail.com>
2023-04-30 13:00:13 -0600

    check

--- stateMachine/miningState.py
+++ stateMachine/miningState.py
@@ -38,27 +38,29 @@ class MiningState(State):
 
         #do face detect stuff 
         faces = None
-        
-        gray = cv2.cvtColor(color_frame, cv2.COLOR_BGR2GRAY)
-        # mat30 = np.full(gray.shape, 30, dtype=np.uint8)
-        # gray = cv2.add(gray, mat30)
-        faces = tango.face_cascade.detectMultiScale(gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-        )
-        
-        faceY = None
-        faceX = None
-        faceW = None
-        faceH = None
+        try:
+            gray = cv2.cvtColor(color_frame, cv2.COLOR_BGR2GRAY)
+            # mat30 = np.full(gray.shape, 30, dtype=np.uint8)
+            # gray = cv2.add(gray, mat30)
+            faces = tango.face_cascade.detectMultiScale(gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+            )
+            
+            faceY = None
+            faceX = None
+            faceW = None
+            faceH = None
 
-        for x, y, w, h in faces:
-            faceY = y
-            faceX = x
-            faceW = w
-            faceH = h
-            cv2.rectangle(color_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            for x, y, w, h in faces:
+                faceY = y
+                faceX = x
+                faceW = w
+                faceH = h
+                cv2.rectangle(color_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        except:
+            print("Face detection not working")
     
 
         if len(yellowContours) > 0:
