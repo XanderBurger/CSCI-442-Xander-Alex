7fbff97c04a563c8128fca67f1ec5765401e916d
Xander Burger <xanburger@hotmail.com>
2023-05-01 16:39:56 -0600

    minor changes

--- stateMachine/startingArea.py
+++ stateMachine/startingArea.py
@@ -45,6 +45,7 @@ class StartingArea(State):
                     self.turnSpeed = 6900
                 elif cX < 400 and cX > 200:
                     self.turnSpeed = 6000
+                
                 if cY < 460:
                     self.forwardSpeed = 5100
                 else:
