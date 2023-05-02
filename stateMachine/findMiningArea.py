b3cf63cc72cfddb95d48d1de4fd8742adcbd64c7
Xander Burger <xanburger@hotmail.com>
2023-05-01 18:15:27 -0600

    back

--- stateMachine/findMiningArea.py
+++ stateMachine/findMiningArea.py
@@ -9,14 +9,14 @@ class FindMiningArea(State):
 
 
     def enterState(self, tango):
-        self.turnSpeed = 5900
+        pass
     
     def process(self, tango, color_frame, depth_frame):
         nextState = None
         mat30 = np.full(color_frame.shape, 30, dtype=np.uint8)
         color_frame = cv2.add(color_frame, mat30)
         corners, ids, rejected = cv2.aruco.detectMarkers(color_frame, self.arucoDict)
-        
+        self.turnSpeed = 5150
 
         try:
             for i in range(len(ids)):
@@ -27,7 +27,7 @@ class FindMiningArea(State):
                     centerY = int((box[1][1] + box[3][1]) / 2)
 
                     if centerX >= 350:
-                        self.turnSpeed = 6900
+                        self.turnSpeed = 5150
                     elif centerX <= 250:
                         self.turnSpeed = 6850
                     elif centerX < 350 and centerX > 250:
@@ -37,10 +37,10 @@ class FindMiningArea(State):
                     cv2.circle(color_frame, (centerX, centerY), 5, (255, 255, 0), 2)
                     cv2.aruco.drawDetectedMarkers(color_frame, corners)
                 else:
-                    self.turnSpeed = 6900
+                    self.turnSpeed = 5150
 
         except TypeError:
-            self.turnSpeed = 6900
+            self.turnSpeed = 5150
             self.forwardSpeed = 6000
             print("NO MARKER FOUND")
         
