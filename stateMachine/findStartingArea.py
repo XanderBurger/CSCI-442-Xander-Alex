1f14df9a1ab55024e16b407a28a589593c85bf47
Xander Burger <xanburger@hotmail.com>
2023-05-02 16:49:22 -0600

    adjust

--- stateMachine/findStartingArea.py
+++ stateMachine/findStartingArea.py
@@ -29,11 +29,11 @@ class FindStartingArea(State):
                     centerX = int((box[0][0] + box[1][0]) / 2)
                     centerY = int((box[1][1] + box[3][1]) / 2)
 
-                    if centerX >= 385:
+                    if centerX >= 390:
                         self.turnSpeed = 5100
-                    elif centerX <= 215:
+                    elif centerX <= 240:
                         self.turnSpeed = 6900
-                    elif centerX < 385 and centerX > 215:
+                    elif centerX < 390 and centerX > 240:
                         self.turnSpeed = 6000
                         return "GO TO START"
                         
