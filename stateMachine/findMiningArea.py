a22aa39498d7dee975ec9477a89f72fc5188a1f2
Xander Burger <xanburger@hotmail.com>
2023-05-03 11:03:27 -0600

    maybe

--- stateMachine/findMiningArea.py
+++ stateMachine/findMiningArea.py
@@ -30,11 +30,11 @@ class FindMiningArea(State):
                     centerX = int((box[0][0] + box[1][0]) / 2)
                     centerY = int((box[1][1] + box[3][1]) / 2)
 
-                    if centerX >= 385:
+                    if centerX >= 390:
                         self.turnSpeed = 5100
-                    elif centerX <= 245:
+                    elif centerX <= 240:
                         self.turnSpeed = 6900
-                    elif centerX < 385 and centerX > 245:
+                    elif centerX < 390 and centerX > 240:
                         self.turnSpeed = 6000
                         return "GO TO MINE"
 
